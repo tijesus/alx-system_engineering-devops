1213f764ac7c209e3670b9855af91ade9f417927
tijesus <emmanueltijesu@gmail.com>
2024-04-19 23:42:47 +0100

    comfiguration system

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # installing flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+command => 'pip3 flask=2.1.0',
+path => '/usr/bin/pip3'
 }
