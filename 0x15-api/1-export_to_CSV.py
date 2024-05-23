57d7afb2ff76f147d0376f920d951bbc00848711
tijesus <emmanueltijesu@gmail.com>
2024-05-23 18:24:13 +0100

    Exporting to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Exporting the employee data to json"""
+""" Exporting the employee data to csv"""
 
 import requests as r
 import sys
@@ -10,12 +10,10 @@ if __name__ == "__main__":
     user_todos_url = url + "/" "todos"
     response = r.get(url).json()
     username = response.get("name")
-    user_todos= r.get(user_todos_url).json()
+    user_todos = r.get(user_todos_url).json()
     with open('{}.csv'.format(user_id), 'w') as file:
         for todo in user_todos:
             done_task_status = todo.get("completed")
             task_title = todo.get("title")
             print('"{}","{}","{}","{}"\n'
                   .format(user_id, username, done_task_status, task_title))
-
-    
