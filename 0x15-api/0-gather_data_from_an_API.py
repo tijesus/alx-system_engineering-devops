7f39548b1c95d7bf3d00f8ed712df4e29c81c625
tijesus <emmanueltijesu@gmail.com>
2024-05-23 16:52:17 +0100

    getting data from api~

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Getting data from an API """
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    user_todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+        .format(user_id)
+    response = requests.get(url).json()
+    username = response.get('name')
+    done_todos = 0
+    user_todos = requests.get(user_todos_url).json()
+    total_todos = len(user_todos)
+    for todo in user_todos:
+        if todo.get("completed"):
+            done_todos += 1
+    print("Employee {} is done with tasks ({}/{}):"
+          .format(username, done_todos, total_todos))
+
+    for todos in user_todos:
+        if todos.get("completed"):
+            print("\t {}".format(todos.get("title")))
